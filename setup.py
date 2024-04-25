3765c8907000bd0e6830b0e344c64b9dff278d63
talgiladi <talgiladi@gmail.com>
2024-04-25 23:35:40 +0300

    3

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import  find_packages, setup
+
+setup(
+    name= "Medical Chatbot",
+    version="1.0.0",
+    author="Tal Giladi",
+    author_email="talgiladi@gmail.com",
+    packages=find_packages(),
+)
